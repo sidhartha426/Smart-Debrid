83fcb16782fc2fd5542155a9b5fffeb7de3da455
Sidhartha Sankar Swain <sidharthasankarswain425@gmail.com>
2023-03-24 18:29:29 +0530

    added init.js

---
+++ init.js
@@ -0,0 +1,49 @@
+import { mkdirSync, readFileSync, writeFileSync } from 'node:fs';
+
+mkdirSync("Scripts/Appauth/Data", { recursive: true });
+mkdirSync("Scripts/Seedbox/Data", { recursive: true });
+mkdirSync("Scripts/Downloader/Data", { recursive: true });
+
+const seedboxFiles = ['add_t_archive.json', 'add_t_history.json'];
+
+const appAuthFiles = ['access_token.json', 'refresh_token.json', 'appinfo.json', 'tokeninfo.json'];
+
+seedboxFiles.forEach((name) => {
+  try {
+    readFileSync("Scripts/Seedbox/Data/" + name)
+  }
+  catch (err) {
+    writeFileSync("Scripts/Seedbox/Data/" + name, JSON.stringify({order:[]}));
+  }
+});
+
+appAuthFiles.forEach((name) => {
+  try {
+    readFileSync("Scripts/Appauth/Data/" + name)
+  }
+  catch (err) {
+    writeFileSync("Scripts/Appauth/Data/" + name, JSON.stringify({}));
+  }
+});
+
+try {
+  readFileSync("Scripts/Seedbox/Data/add_t_queue.json")
+}
+catch (err) {
+  writeFileSync("Scripts/Seedbox/Data/add_t_queue.json", JSON.stringify([])); 
+}
+
+try {
+  readFileSync("Scripts/Seedbox/Data/testdata.json")
+}
+catch (err) {
+  writeFileSync("Scripts/Seedbox/Data/testdata.json", JSON.stringify({})); 
+}
+
+try {
+  readFileSync("Scripts/Downloader/Data/testdata.json")
+}
+catch (err) {
+  writeFileSync("Scripts/Downloader/Data/testdata.json", JSON.stringify({})); 
+}
+
